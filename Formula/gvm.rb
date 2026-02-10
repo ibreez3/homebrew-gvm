d24580206f1a7131823a6bdf75d44f55a9d77ad6
sunyang <sunyang@bcds.org.cn>
2026-02-10 10:49:01 +0800

    Update gvm to v0.2.1

--- Formula/gvm.rb
+++ Formula/gvm.rb
@@ -1,12 +1,11 @@
 # Homebrew formula for gvm
-# Repository: https://github.com/ibreez3/homebrew-gvm
-# To install: brew tap ibreez3/gvm && brew install gvm
+# To install: brew install ibreez3/gvm/gvm
 
 class Gvm < Formula
   desc "Go Version Manager - Manage multiple Go versions easily"
   homepage "https://github.com/ibreez3/gvm"
-  url "https://github.com/ibreez3/gvm/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 :no_check # Will be updated on release
+  url "https://github.com/ibreez3/gvm/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "2b0e04bf365da05171e8b23bd2de80730c362dd4651fcf49b5c860a507fdf920"
   license "MIT"
 
   depends_on "go" => :build
